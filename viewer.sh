35b23737cdd8ee7f653aeb1812b3b36caa1210fe
Will Chen <willchen90@gmail.com>
2025-04-01 23:41:42 -0700

    Use uv for random generation

--- viewer.sh
+++ viewer.sh
@@ -1,3 +1,3 @@
 # Used to start viewer server (in prod).
 # Avoid over-logging.
-MESOP_HTTP_CACHE_JS_BUNDLE=true MESOP_WEB_COMPONENTS_HTTP_CACHE_KEY=$(awk 'BEGIN { srand(); print int(rand() * 10^18) }') DYAD_LOG_LEVELS=WARNING DYAD_APP_MODE=viewer DYAD_WORKSPACE_DIR=. MESOP_STATIC_FOLDER=./src/dyad_app/static uv run gunicorn viewer:mesop
+MESOP_HTTP_CACHE_JS_BUNDLE=true MESOP_WEB_COMPONENTS_HTTP_CACHE_KEY=$(echo 'import random; print(random.randint(0, 10**18 - 1))' | uv run -) DYAD_LOG_LEVELS=WARNING DYAD_APP_MODE=viewer DYAD_WORKSPACE_DIR=. MESOP_STATIC_FOLDER=./src/dyad_app/static uv run gunicorn viewer:mesop
