68190f4ce60cd3deefaf17e16199fbf6bb578117
Will Chen <willchen90@gmail.com>
2025-04-01 23:08:33 -0700

    Add comment on viewer.sh (#14)

--- viewer.sh
+++ viewer.sh
@@ -1,2 +1,3 @@
+# Used to start viewer server (in prod).
 # Avoid over-logging.
 MESOP_HTTP_CACHE_JS_BUNDLE=true MESOP_WEB_COMPONENTS_HTTP_CACHE_KEY=$(git rev-parse HEAD) DYAD_LOG_LEVELS=WARNING DYAD_APP_MODE=viewer DYAD_WORKSPACE_DIR=. MESOP_STATIC_FOLDER=./src/dyad_app/static uv run gunicorn viewer:mesop
